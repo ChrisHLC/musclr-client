32ea6ebcd0bd29b9395ca7f37454f204e6d83c08
rukiyeg <r.gungoroglu@gmail.com>
2018-05-16 21:50:25 +0200

    legend basis

--- src/app/components/dashboard/graph-display/graph-legend/graph-legend.component.ts
+++ src/app/components/dashboard/graph-display/graph-legend/graph-legend.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-graph-legend',
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraphLegendComponent implements OnInit {
 
+  @Input()
+  checkModel;
+  
   constructor() { }
 
   ngOnInit() {
